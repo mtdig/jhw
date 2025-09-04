be7cff0cf599991aeac37aecf51a9a82d1fd2a19
jeroenflvr <jeroen@flexworks.eu>
2025-09-04 21:07:25 +0200

    init

---
+++ src/main/java/eu/flexworks/processor/Main.java
@@ -0,0 +1,9 @@
+
+package eu.flexworks.processor;
+import helpers.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+    Helper.printMsg("Cool");
+    }
+}
---
+++ src/main/java/helpers/Helper.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public class Helper {
+    public static void printMsg(String msg) {
+        System.out.println("var passed from helpers package: " + msg);
+    }
+}
